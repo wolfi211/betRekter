e8ff0142cc0b012f0cdd1f27f7a996245c924537
Wolfi211 <wolf.dani9@gmail.com>
2015-07-13 12:47:21 +0200

    removed debug folder from git, continued the MATCH Class structure

--- tryouts1.1/Team.cs
+++ tryouts1.1/Team.cs
@@ -8,6 +8,111 @@ namespace tryouts1._1
 {
     class Team
     {
+        private string name;
+        private string urlFacebook;
+        private string urlTwitter;
+        private string urlHltv;
+        private Player[] players = new Player[25];
+        private int cnt = 0;
+        private int cnt2 = 0;
 
+        #region CONSTRUCTORS
+        public Team()
+        {
+
+        }
+
+        public Team(string s)
+        {
+            name = s;
+        }
+
+        public Team(string s, string url1, string url2, string url3)
+        {
+            name = s;
+            urlTwitter = url1;
+            urlFacebook = url2;
+            urlHltv = url3;
+        }
+        #endregion
+
+        #region GET_SET_METHODS
+
+        #region GET
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getTwitter()
+        {
+            return urlTwitter;
+        }
+
+        public string getFacebook()
+        {
+            return urlFacebook;
+        }
+
+        public string getHltv()
+        {
+            return urlHltv;
+        }
+
+        public Player getPlayer(int i)
+        {
+            return players[i];
+        }
+
+        public Player getNextPlayer()
+        {
+            if (cnt2 == 25) cnt2 = 0;
+            return players[cnt2++];
+        }
+        #endregion
+
+        #region SET
+        public void setName(string s)
+        {
+            name = s;
+        }
+
+        public void setTwitter(string s)
+        {
+            urlTwitter = s;
+        }
+
+        public void setFacebook(string s)
+        {
+            urlFacebook = s;
+        }
+
+        public void setHltv(string s)
+        {
+            urlHltv = s;
+        }
+
+        public void addPlayer(Player p)
+        {
+            if (cnt == 25) cnt = 0;
+            players[cnt++] = p;
+        }
+
+        public void setPlayer(Player p, int i)
+        {
+            players[i] = p;
+        }
+        #endregion
+        #endregion
+
+        public void resetCounter()
+        {
+            cnt = 0;
+        }
+
+        public void resetCounter2()
+        {
+            cnt2 = 0;
+        }
     }
 }
